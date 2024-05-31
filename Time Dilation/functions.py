268c246acb3dd79a18664b5e7187c13a9a4b6585
Kiril Lazarov <kirillazarov@abv.bg>
2024-05-31 20:31:23 +0300

    Added markers list variable

--- Time Dilation/functions.py
+++ Time Dilation/functions.py
@@ -10,11 +10,18 @@ def create_background(screen_width, screen_height, bg_color, units, length):
     font_small = pygame.font.Font(None, 26)
     background_surface.fill(bg_color)
     
+    markers = []
+    
     # Coordinate x-line parameters
     x_line_start = screen_width-1400
     x_line_end = screen_width-100
     y_line = screen_height-150    
     line_color = (185, 185, 185)
+    
+    markers.append({
+        'receiver_x': x_line_end,
+        'receiver_y': y_line
+    })
     
     pygame.draw.line(background_surface, color = line_color, start_pos=(x_line_start, y_line),
                          end_pos = (x_line_end, y_line), width=2)
@@ -37,7 +44,7 @@ def create_background(screen_width, screen_height, bg_color, units, length):
     description = font_small.render(f'Distance in light seconds', True, line_color)
     background_surface.blit(description, (x_line_start+550, y_line+50))
     
-    return background_surface
+    return background_surface, markers
 
 
 def update_first_light_front(first_light_front, displacement_per_frame):
@@ -49,7 +56,7 @@ def gamma_factor(v):
     return np.sqrt(1 - v ** 2)
 
 # Update the coordinates of the messages emitted by the spaceship that are shown on the screen.
-def update_message_position(vis_angle, space_ship_pos, signal):
+def update_message_position(vis_angle, signal):
     
     x = signal.origin_position[0] + signal.radius    
     y= signal.origin_position[1] - 50
